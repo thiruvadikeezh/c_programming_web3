a907caef50c6568aab3a73e37a24f8452cac7685
thiruvadikeezh <appubaktha@gmail.com>
2026-02-16 01:19:48 +0530

    cmake list inside

---
+++ book/build/CMakeFiles/temp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temp.dir/temp.c.o"
+  "CMakeFiles/temp.dir/temp.c.o.d"
+  "temp"
+  "temp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/temp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
