6868db70856f4bfd6dd2693da93255ffd24bb567
thiruvadikeezh <appubaktha@gmail.com>
2026-02-16 23:44:22 +0530

    intro into arrays

--- book/temp.c
+++ book/temp.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
-
-#define IN 1
-#define OUT 0
-
 int main()
 {
-	int c, nl, nw, nc, state;
+	int c, i, nwhite, nother;
+	int ndigit[10];
 
-	state = OUT ;
+	nwhite = nother = 0;
+	for( i= 0; i<10; ++i)
+		ndigit[i] = 0;
 
-	nl = nw = nc = 0;
+	while(( c = getchar()) != EOF)
+		if( c<= '0' && c<= '9')
+			++ndigit[c-'0'];
+		else if ( c == ' ' || c =='\n' || c =='\t')
+			++nwhite;
+		else
+			++nother;
 
-	while((c = getchar()) != EOF) {
-		++nc;
+	printf("digits=");
+	for ( i= 0; i<10; ++i)
+		printf( " %0d", ndigit[i]);
 
-		if ( c == '\n')
-			++nl;
-		if ( c == ' ' || c== '\n' || c == '\t')
-			state = OUT;
-		else if (state == OUT)
-			state = IN;
-			++nw;
-	}
-	printf("%d %d %d\n", nl, nw, nc);
+	printf(", white space = %d, other = %d\n", nwhite,nother);
 }
-
-
 
 
 
