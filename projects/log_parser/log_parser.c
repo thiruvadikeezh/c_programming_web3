0918904384c6910da0eded3f67acd55ad0530aae
sudh-lap <appubaktha@gmail.com>
2026-03-20 08:05:06 +0530

    adding array indexing

--- projects/log_parser/log_parser.c
+++ projects/log_parser/log_parser.c
@@ -26,6 +26,7 @@ LogState parse_log_file(const char* filename)
 
 		if (strstr(line, "[INFO]")){
 			log.info_count++;
+
 		} else if (strstr(line, "[ERROR]")){
 			log.error_count++;
 		} else if (strstr(line, "[WARNING]")){
@@ -56,15 +57,30 @@ int parse_the_file(const char* filename, LogState *out)
 
 	*out = (LogState){0}; //initialize
 	
+
+	int line_number = 0;
+	
 	while (fgets(line, sizeof(line), file))
 	{
+		line_number++;
+		
 		if(strstr(line, "[INFO]"))
+		{
 			out->info_count++;
+			out->info_log_lines[out->info_count] = line_number;
+		}
 		else if (strstr(line, "[WARNING]"))
+		{
 			out->warning_count++;
+			out->error_log_lines[out->warning_count] = line_number;
+		}
 		else if (strstr(line, "[ERROR]"))
+		{
 			out->error_count++;
+			out->warning_log_lines[out->error_count] = line_number;
+		}
 	}
+
 
 	fclose(file);
 	return 0; //success;
@@ -74,8 +90,29 @@ int parse_the_file(const char* filename, LogState *out)
 
 void display_logs(LogState *msg)
 {
-	printf("info:\n");
-	printf("error:\n");
-	printf("warning:\n");
+	int i = 0;
+	
+	printf("INFO:\n");
+	for(i ; i<msg->info_count; i++)
+	{
+		printf("Line: %d\n", msg->info_log_lines[i]);
+	}
+	
+	printf("ERROR:\n");
+
+	for(i ; i<msg->error_count; i++)
+	{
+		printf("Line: %d\n", msg->error_log_lines[i]);
+
+	}
+	
+	printf("Warning:\n");
+	
+	for(i; i<msg->warning_count; i++)
+	{
+		printf("Line: %d\n", msg->warning_log_lines[i]);
+	}
+
+
 }
 
--- projects/log_parser/log_parser.h
+++ projects/log_parser/log_parser.h
@@ -1,13 +1,22 @@
 #ifndef LOG_PARSER_H
 #define LOG_PARSER_H
 
-
+#define MAX 10
 
 typedef struct
 {
 	int info_count;
 	int error_count;
 	int warning_count;
+	
+	int info_log_lines[MAX];
+	int error_log_lines[MAX];
+	int warning_log_lines[MAX];
+
+	int info_index;
+	int error_index;
+	int warning_index;
+
 }LogState;
 
 LogState parse_log_file(const char* filename);
