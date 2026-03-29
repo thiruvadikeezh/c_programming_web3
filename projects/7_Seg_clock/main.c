b4c417e26e4b7e2c84fe97581c89a055574342d0
sudh-lap <appubaktha@gmail.com>
2026-03-29 22:16:05 +0530

    complete 7 segment

--- projects/7_Seg_clock/main.c
+++ projects/7_Seg_clock/main.c
@@ -3,50 +3,39 @@
 #include <stdbool.h>
 #include "raylib.h"
 
-
+// definign the windows 
 #define WIDTH  1000
 #define HEIGHT 1000
 
+// defining the segment led sizes
+#define segment_width  50
+#define segment_height 10
+#define segment_thickness  10
+
+float hor_off = segment_width;
+
+// defining the hours offset
 Vector2 hours []={
-    (Vector2){100, 100},
-    (Vector2){225, 100}
+    (Vector2){100, 400},
+    (Vector2){250, 400}
 };
 
-
-float segment_width = 100;
-float segment_height = 20;
-float segment_thickness = 20;
-
-void DrawSegment(Vector2 center, bool horizontal)
+// function for horizontal Segment which is top block 
+void HorizontalSegment(Vector2 center)
 {
         int count = 6;
 
         Vector2 a, b, c, d, e, f;
-        
+ 
         float left_edge_hori = center.x - segment_width/2 - segment_thickness/2;
         float right_edge_hori = center.x + segment_width/2 + segment_thickness/2;
 
-        if (horizontal)
-        {
-            a = (Vector2){left_edge_hori, center.y};
-            b = (Vector2){center.x - segment_width/2, center.y + segment_height/2};
-            c = (Vector2){center.x - segment_width/2, center.y - segment_height/2};
-            d = (Vector2){center.x + segment_width/2, center.y + segment_height/2};
-            e = (Vector2){center.x + segment_width/2, center.y - segment_height/2};
-            f = (Vector2){right_edge_hori, center.y};
-        }
-
-        else
-        {
-
-            a = (Vector2){center.x - segment_width/2 - segment_thickness/2, center.y};
-            b = (Vector2){center.x - segment_width/2 - segment_thickness, center.y + segment_height/2};
-            c = (Vector2){center.x - segment_width/2 , center.y + segment_height/2};
-            d = (Vector2){center.x - segment_width/2 - segment_thickness, center.y + segment_width - segment_thickness/2};
-            e = (Vector2){center.x - segment_width/2 , center.y + segment_width - segment_thickness/2};
-            f = (Vector2){center.x - segment_width/2 - segment_thickness/2, center.y + segment_width};
-        }
-
+        a = (Vector2){left_edge_hori, center.y};
+        b = (Vector2){center.x - segment_width/2, center.y + segment_height/2};
+        c = (Vector2){center.x - segment_width/2, center.y - segment_height/2};
+        d = (Vector2){center.x + segment_width/2, center.y + segment_height/2};
+        e = (Vector2){center.x + segment_width/2, center.y - segment_height/2};
+        f = (Vector2){right_edge_hori, center.y};
 
 
         Vector2 seg[] = {a, b, c, d, e, f};
@@ -54,6 +43,29 @@ void DrawSegment(Vector2 center, bool horizontal)
 
 }
 
+// function for vertical block which has left and rigth segment
+void VerticalSegment(Vector2 center)
+{
+    int count = 6;
+    Vector2 a, b, c, d, e, f;
+
+    float rig_seg = segment_width + segment_thickness;
+    for (int i =0; i <2; i++)
+    {
+
+        a = (Vector2){(center.x - segment_width/2 - segment_thickness/2)+i * rig_seg, center.y};
+        b = (Vector2){(center.x - segment_width/2 - segment_thickness)+i* rig_seg, center.y + segment_height/2};
+        c = (Vector2){(center.x - segment_width/2)+i* rig_seg, center.y + segment_height/2};
+        d = (Vector2){(center.x - segment_width/2 - segment_thickness)+i* rig_seg, center.y + segment_width - segment_thickness/2};
+        e = (Vector2){(center.x - segment_width/2)+i* rig_seg, center.y + segment_width - segment_thickness/2};
+        f = (Vector2){(center.x - segment_width/2 - segment_thickness/2)+i* rig_seg, center.y + segment_width};
+
+        Vector2 seg[] = {a, b, c, d, e, f};
+        DrawTriangleStrip(seg, count, RAYWHITE);
+    }
+            
+}
+//drawing the horizontal lines
 void DrawHorizontal(Vector2 *off_array, int off_level)
 {
     Vector2 base = off_array[off_level];
@@ -62,9 +74,32 @@ void DrawHorizontal(Vector2 *off_array, int off_level)
     {
         Vector2 center = {
             base.x,
-            base.y + i * 100
+            base.y + i * hor_off
+            };
+        HorizontalSegment(center);
+    }
+}
+
+void DrawVertical(Vector2 *off_array, int off_level)
+{
+    Vector2 base = off_array[off_level];
+
+    for(int i = 0; i < 2; i++)
+    {
+        Vector2 center = {
+            base.x,
+            base.y + i * hor_off
         };
-        DrawSegment(center, true);
+        VerticalSegment(center);
+    }
+}
+
+void DrawSegment(Vector2 *center)
+{
+    for(int i = 0; i < 2; i++)
+    {
+        DrawHorizontal(center, i);
+        DrawVertical(center, i);
     }
 }
 
@@ -84,12 +119,10 @@ int main(int argc, char *argv[])
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // DrawSegment(center, 0);
-        // DrawSegment(center, 1);
-        DrawHorizontal(hours, 0);
+        DrawSegment(hours);
+
         EndDrawing();
     }
 
 	return 0;
 }
-
