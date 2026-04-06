bebc12179d6235b982a0385b630a4fa3dc4deff6
sudh-lap <appubaktha@gmail.com>
2026-04-06 22:39:55 +0530

    made simple horizontal and vertical segments next needed to make an seperate function called draw segment which will ahve clear 7 segment drawing elements

--- projects/7_Seg_clock/main.c
+++ projects/7_Seg_clock/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
+
+// lib for crawing vector graphics
 #include "raylib.h"
 
 // definign the windows 
@@ -8,20 +10,17 @@
 #define HEIGHT 1000
 
 // defining the segment led sizes
-#define segment_width  50
-#define segment_height 10
-#define segment_thickness  10
-
-float hor_off = segment_width;
+#define segment_width 200 
+#define segment_height 50
+#define segment_thickness 50
 
 // defining the hours offset
 Vector2 hours []={
-    (Vector2){100, 400},
-    (Vector2){250, 400}
+    (Vector2){500, 500}
 };
 
 // function for horizontal Segment which is top block 
-void HorizontalSegment(Vector2 center)
+void draw_horizontal(Vector2 center)
 {
         int count = 6;
 
@@ -39,69 +38,37 @@ void HorizontalSegment(Vector2 center)
 
 
         Vector2 seg[] = {a, b, c, d, e, f};
-        DrawTriangleStrip(seg, count, RAYWHITE);
+        DrawTriangleStrip(seg, count, LIME);
 
 }
 
 // function for vertical block which has left and rigth segment
-void VerticalSegment(Vector2 center)
+void draw_vertical(Vector2 center)
 {
+
     int count = 6;
+
     Vector2 a, b, c, d, e, f;
 
-    float rig_seg = segment_width + segment_thickness;
-    for (int i =0; i <2; i++)
-    {
+    a = (Vector2){(center.x - segment_width/2 - segment_thickness/2), center.y};
+    b = (Vector2){(center.x - segment_width/2 - segment_thickness), center.y + segment_height/2};
+    c = (Vector2){(center.x - segment_width/2), center.y + segment_height/2};
+    d = (Vector2){(center.x - segment_width/2 - segment_thickness), center.y + segment_width - segment_thickness/2};
+    e = (Vector2){(center.x - segment_width/2), center.y + segment_width - segment_thickness/2};
+    f = (Vector2){(center.x - segment_width/2 - segment_thickness/2), center.y + segment_width};
 
-        a = (Vector2){(center.x - segment_width/2 - segment_thickness/2)+i * rig_seg, center.y};
-        b = (Vector2){(center.x - segment_width/2 - segment_thickness)+i* rig_seg, center.y + segment_height/2};
-        c = (Vector2){(center.x - segment_width/2)+i* rig_seg, center.y + segment_height/2};
-        d = (Vector2){(center.x - segment_width/2 - segment_thickness)+i* rig_seg, center.y + segment_width - segment_thickness/2};
-        e = (Vector2){(center.x - segment_width/2)+i* rig_seg, center.y + segment_width - segment_thickness/2};
-        f = (Vector2){(center.x - segment_width/2 - segment_thickness/2)+i* rig_seg, center.y + segment_width};
-
-        Vector2 seg[] = {a, b, c, d, e, f};
-        DrawTriangleStrip(seg, count, RAYWHITE);
-    }
-            
-}
-//drawing the horizontal lines
-void DrawHorizontal(Vector2 *off_array, int off_level)
-{
-    Vector2 base = off_array[off_level];
-
-    for(int i = 0; i < 3; i++)
-    {
-        Vector2 center = {
-            base.x,
-            base.y + i * hor_off
-            };
-        HorizontalSegment(center);
-    }
+    Vector2 seg[] = {a, b, c, d, e, f};
+    DrawTriangleStrip(seg, count, SKYBLUE);
+                
 }
 
-void DrawVertical(Vector2 *off_array, int off_level)
-{
-    Vector2 base = off_array[off_level];
-
-    for(int i = 0; i < 2; i++)
-    {
-        Vector2 center = {
-            base.x,
-            base.y + i * hor_off
-        };
-        VerticalSegment(center);
-    }
-}
 
 void DrawSegment(Vector2 *center)
 {
-    for(int i = 0; i < 2; i++)
-    {
-        DrawHorizontal(center, i);
-        DrawVertical(center, i);
-    }
+    draw_horizontal(* center);
+    draw_vertical(* center);
 }
+
 
 
 int main(int argc, char *argv[])
