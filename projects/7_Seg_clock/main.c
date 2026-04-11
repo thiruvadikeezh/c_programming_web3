732d0e735959072101bba145b8d577e6a373255d
sudh-lap <appubaktha@gmail.com>
2026-04-11 17:03:21 +0530

    finally the time has been implemented int he segment display

--- projects/7_Seg_clock/main.c
+++ projects/7_Seg_clock/main.c
@@ -15,27 +15,38 @@
 
 int main(int argc, char *argv[])
 {
-	time_t current_time = time(NULL);
-	char *time_string = ctime(&current_time);
-
-
-	printf("the current time is : %s\n", time_string);
     InitWindow(WIDTH, HEIGHT, "7-Seg-Clock");
-
 
     while (!WindowShouldClose())
     {
+        
+        time_t current_time = time(NULL);
+        struct tm *local = localtime(&current_time);
+        
+        int hrs = local->tm_hour;
+        int min = local->tm_min;
+        int sec = local->tm_sec;
+
+        int h1 = hrs / 10;
+        int h2 = hrs % 10;
+
+        int m1 = min / 10;
+        int m2 = min % 10;
+
+        int s1 = sec / 10;
+        int s2 = sec % 10;
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        draw_segment(&hours[0], 0);
-        draw_segment(&hours[1], 1);
+        draw_segment(&hours[0], h1);
+        draw_segment(&hours[1], h2);
 
-        draw_segment(&minutes[0], 2);
-        draw_segment(&minutes[1], 3);
+        draw_segment(&minutes[0], m1);
+        draw_segment(&minutes[1], m2);
 
-        draw_segment(&seconds[0], 4);
-        draw_segment(&seconds[1], 5);
+        draw_segment(&seconds[0], s1);
+        draw_segment(&seconds[1], s2);
 
         EndDrawing();
     }
