3bf7a707c41079490c01c7bc7584a87a4990ba90
sudh-lap <appubaktha@gmail.com>
2026-04-14 16:23:15 +0530

    linked list

--- dsa/ll_list/main.c
+++ dsa/ll_list/main.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct node{
     int val;
     struct node *next_node;
 }node;
 
-
-void create_node(node *n, int num)
+node* create_node(int num)
 {
-    *n = malloc(sizeof(struct node));
+    node *n = malloc(sizeof(node));
 
-    (*n)->val = num;
-    (*n)->next_node = NULL;
+    n->val = num;
+    n->next_node = NULL;
 
+    return n;
 }
 
 void add_node(node *n1, node *n2)
 {
-    (*n1)->next_node = (*n2);
-
+    n1->next_node = n2;
 }
+
 
 int main()
 {
-    node n1;
-    node n2;
 
-    create_node(&n1, 10);
-    create_node(&n2, 20);
+    node *n1 = create_node(10);
+    node *n2 = create_node(20);
 
-    add_node(&n1, &n2);
+    add_node(n1, n2);
+
+    printf("\n n1 val %d\n", n1->val);
+
+    printf("\n n2 val %d\n", n2->val);
+
+    free(n1);
+    free(n2);
 
     return 0;
 
